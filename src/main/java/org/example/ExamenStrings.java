c6db2610985a719c2a51105e849b911a744f6bf2
Mario Torrús <mariotorrus04@gmail.com>
2022-12-15 22:25:59 +0100

    Examen Strings ex4  indexOutOfBounds

--- src/main/java/org/example/ExamenStrings.java
+++ src/main/java/org/example/ExamenStrings.java
@@ -5,15 +5,15 @@ import java.util.Arrays;
 public class ExamenStrings {
     public static void main(String[] args) {
         String provaExecucio = "Cuando un tonto coje un cam)ino, se acaba el camino y sigue el tonto!. Mario dixit";
+        char[] delimitadorsDefinits = {' ', '-', ',', '.', '!', '?', '\''};
         System.out.println(stringAMatriuChar(provaExecucio));
         ExamenStrings.printMatriuChar(provaExecucio);
-
-        char[] M = {'1','f','h'};
-        System.out.println(M);
+        System.out.println(delimitadorDefinit(provaExecucio, delimitadorsDefinits, 2));
 
     }
+
     // 2
-    public static char[] stringAMatriuChar (String provaExecucio){
+    public static char[] stringAMatriuChar(String provaExecucio) {
         String lowerCaseString = provaExecucio.toLowerCase();
         char[] matriuChars = new char[lowerCaseString.length()];
         //char resultat;
@@ -32,9 +32,22 @@ public class ExamenStrings {
 
     // 3
 
-    public static void printMatriuChar (String provaExecucio) {
+    public static void printMatriuChar(String provaExecucio) {
         System.out.println(stringAMatriuChar(provaExecucio));
     }
 
+    // 4
+    public static boolean delimitadorDefinit(String provaExecucio, char[] delimitadorsDefinits, int posicio) {
+        boolean resultat = false;
+        for (int i = 0; i < provaExecucio.length() - 1; i++) {
+            if (posicio <= provaExecucio.length()) {
+                if (provaExecucio.charAt(posicio + i) == delimitadorsDefinits[i]) {
+                    resultat = true;
+                    break;
+                }
+            }
+        }
+        return resultat;
+    }
 
 }
